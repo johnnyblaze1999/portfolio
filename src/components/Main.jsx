95bae56f07bd03d28218e5cec167e1ccb4ea2a3c
Kevin Nguyen <johnnyblaze1999.sg@gmail.com>
2024-03-08 19:27:10 -0800

    Update text alignment

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,8 +8,8 @@ const Main = () => {
         <img className='w-full h-screen object-cover object-left' src='https://images.unsplash.com/photo-1622819584099-e04ccb14e8a7?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
         <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
             <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
-                <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>
-                    My name is <span className='rounded-xl bg-white shadow-md pl-2 pb-2'><TypeAnimation
+                <h1 className='sm:text-5xl text-center text-4xl font-bold text-gray-800'>
+                    My name is <span className=' rounded-xl bg-white shadow-md pl-2 pb-2'><TypeAnimation
                     sequence={[
                         'Kevin Nguyen',
                         5000, 
