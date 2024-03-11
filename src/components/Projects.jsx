e0b700bc0b8b3e9787cf0a502c43339613470c9e
Kevin Nguyen <johnnyblaze1999.sg@gmail.com>
2024-03-10 23:50:54 -0700

    Update project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,10 +16,11 @@ const Projects = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
         <div className='grid sm:grid-cols-2 gap-12'>
-            <ProjectItem img={placeholderImg} title = 'First Project'/>
-            <ProjectItem img={placeholderImg} title = 'Second Project'/>
-            <ProjectItem img={placeholderImg} title = 'Third Project'/>
-            <ProjectItem img={placeholderImg} title = 'Fourth Project'/>
+            <ProjectItem img={placeholderImg} title = 'Personal Portfolio'/>
+            <ProjectItem img={placeholderImg} title = 'Fullyhack Chrome Extension'/>
+            <ProjectItem img={placeholderImg} title = 'BotPress Chatbot'/>
+            <ProjectItem img={placeholderImg} title = 'School Database'/>
+            <ProjectItem img={placeholderImg} title = 'Hotel Webpage'/>
         </div>
     </div>
   )
