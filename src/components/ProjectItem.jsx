a168041bec28f38ccb8b5dc98403aa104deed6bc
Kevin Nguyen <johnnyblaze1999.sg@gmail.com>
2024-03-16 21:16:16 -0700

    Updated details for project items

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
-const ProjectItem = ({img, title}) => {
+const ProjectItem = ({img, title, description}) => {
   return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-tr from-gray-400 to-emerald-400'>
-        <img src={img}/>
+    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-tr from-gray-400 to-emerald-400 hover:scale-110'>
+        <img src={img} alt='/' className='rounded-xl object-cover h-96 w-96 group-hover:opacity-10 '/>
         <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
             <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
                 {title}
+                <details className="rounded-lg hover:cursor-pointer" open>
+                  <summary className="text-sm select-none">
+                    Details
+                  </summary>
+                  <div className="mt-1 text-sm">
+                    {description}
+                  </div>
+                </details>
             </h3>
         </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ProjectItem from './ProjectItem'
 import placeholderImg from '../assets/placeholder.png'
+import gradebookImg from '../assets/gradebookProject.png'
+import aichatbotImg from '../assets/aichatbotProject.png'
 
 /** import propertyImg from '../assets/property.jpg'
 import cryptoImg from '../assets/crypto.jpg'
@@ -13,16 +15,16 @@ const Projects = () => {
         <h1 className='text-4xl font-bold text-center text-[#001b5e]'>
             Projects
         </h1>
-        <p className='py-8'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <p className='text-center py-8'>
+            Here are the projects that I've done over the years
         </p>
         <div className='grid sm:grid-cols-2 gap-12'>
-            <ProjectItem img={placeholderImg} title = 'Personal Portfolio'/>
-            <ProjectItem img={placeholderImg} title = 'Unity Game'/>
-            <ProjectItem img={placeholderImg} title = 'Fullyhack Chrome Extension'/>
-            <ProjectItem img={placeholderImg} title = 'BotPress Chatbot'/>
-            <ProjectItem img={placeholderImg} title = 'School Database'/>
-            <ProjectItem img={placeholderImg} title = 'Hotel Webpage'/>
+            <ProjectItem img={placeholderImg} title = 'Personal Portfolio' description='My personal portfolio webpage showcasing my skills, experiences, and projects.'/>
+            <ProjectItem img={placeholderImg} title = 'Unity Game' description='A fun desktop game where the player dodges incoming projectiles and survive under the time limit.'/>
+            <ProjectItem img={placeholderImg} title = 'Fullyhack Chrome Extension' description="A chrome extension that return the weather status in real-time base on the given city's name."/>
+            <ProjectItem img={aichatbotImg} title = 'BotPress Chatbot' description="AI Chatbot that use a given dataset along with clever prompt engineering to assist customers for a quicker and better customer service."/>
+            <ProjectItem img={gradebookImg} title = 'Gradebook Database' description="Database system designed to assist professors and students on keeping track of their courses, grades, class's session, and standing status."/>
+            <ProjectItem img={placeholderImg} title = 'Hotel Webpage' description="A simple styled Hotel website to support guests on seeking information and hotel's schedule."/>
         </div>
     </div>
   )
