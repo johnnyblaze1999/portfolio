0253520e5122572e9e940609c1767eada96cdad7
Kevin Nguyen <johnnyblaze1999.sg@gmail.com>
2024-03-18 19:42:37 -0700

    Update projectItem

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,15 +16,15 @@ const Projects = () => {
             Projects
         </h1>
         <p className='text-center py-8'>
-            Here are the projects that I've done over the years
+            Here are some of the projects that I've done over the years. Feel free to check around more projects in my Github repo.
         </p>
         <div className='grid sm:grid-cols-2 gap-12'>
-            <ProjectItem img={placeholderImg} title = 'Personal Portfolio' description='My personal portfolio webpage showcasing my skills, experiences, and projects.'/>
-            <ProjectItem img={placeholderImg} title = 'Unity Game' description='A fun desktop game where the player dodges incoming projectiles and survive under the time limit.'/>
-            <ProjectItem img={placeholderImg} title = 'Fullyhack Chrome Extension' description="A chrome extension that return the weather status in real-time base on the given city's name."/>
-            <ProjectItem img={aichatbotImg} title = 'BotPress Chatbot' description="AI Chatbot that use a given dataset along with clever prompt engineering to assist customers for a quicker and better customer service."/>
-            <ProjectItem img={gradebookImg} title = 'Gradebook Database' description="Database system designed to assist professors and students on keeping track of their courses, grades, class's session, and standing status."/>
-            <ProjectItem img={placeholderImg} title = 'Hotel Webpage' description="A simple styled Hotel website to support guests on seeking information and hotel's schedule."/>
+            <ProjectItem img={placeholderImg} title = 'Personal Portfolio' description='My personal portfolio webpage showcasing my skills, experiences, and projects.' url='/' tools=''/>
+            <ProjectItem img={placeholderImg} title = 'Unity Game' description='A fun desktop game where the player dodges incoming projectiles and survive under the time limit.' url='/' tools=''/>
+            <ProjectItem img={placeholderImg} title = 'Fullyhack Chrome Extension' description="A chrome extension that return the weather status in real-time base on the given city's name. This is a 24h project for FullyHack" url='/' tools=''/>
+            <ProjectItem img={aichatbotImg} title = 'BotPress Chatbot' description="AI Chatbot that use a given dataset along with clever prompt engineering to assist customers for a quicker and better customer service." url='/' tools=''/>
+            <ProjectItem img={gradebookImg} title = 'Gradebook Database' description="Database system designed to assist professors and students on keeping track of their courses, grades, class's session, and standing status." url='https://github.com/johnnyblaze1999/Gradebook-database' tools=''/>
+            <ProjectItem img={placeholderImg} title = 'Hotel Webpage' description="A simple styled Hotel website to support guests on seeking information and hotel's schedule." url='/' tools=''/>
         </div>
     </div>
   )
