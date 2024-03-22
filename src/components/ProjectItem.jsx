6b43f6c10ed5a6f3c1b134322360a790f300f840
Kevin Nguyen <johnnyblaze1999.sg@gmail.com>
2024-03-21 22:04:19 -0700

    Update portfolio

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const ProjectItem = ({img, title, description, url, tools}) => {
+  const toolList = tools.split(', ');
+
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-tr from-gray-400 to-emerald-400 hover:scale-110'>
         <img src={img} alt='/' className='rounded-xl object-cover h-96 w-96 group-hover:opacity-10 '/>
@@ -8,8 +10,15 @@ const ProjectItem = ({img, title, description, url, tools}) => {
             <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
                 {title}
             </h3>
+            <div className='flex justify-center space-x-2'>
+              {toolList.map((tool, index) => (
+                <button key={index} className='px-2 my-2 py-1 bg-rose-400 text-white text-sm rounded-full'>{tool}</button>
+              ))}
+            </div>
             <a href={url} target='_blank' rel='noopener norefferer'>
-              <p className='text-center p-1 my-2 rounded-lg bg-white text-gray-700 font-bold cursor-pointer'>View Project</p>
+              <p className='text-center p-1 my-2 rounded-lg bg-white text-gray-700 font-bold cursor-pointer transition hover:scale-110 ease-in active:scale-90'>
+                View Project
+              </p>
             </a>
             <details className="text-white text-center font-semibold rounded-lg hover:cursor-pointer" close>
               <summary className="text-sm select-none">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,11 +19,11 @@ const Projects = () => {
             Here are some of the projects that I've done over the years. Feel free to check around more projects in my Github repo.
         </p>
         <div className='grid sm:grid-cols-2 gap-12'>
-            <ProjectItem img={placeholderImg} title = 'Personal Portfolio' description='My personal portfolio webpage showcasing my skills, experiences, and projects.' url='/' tools=''/>
-            <ProjectItem img={placeholderImg} title = 'Unity Game' description='A fun desktop game where the player dodges incoming projectiles and survive under the time limit.' url='/' tools=''/>
-            <ProjectItem img={placeholderImg} title = 'Fullyhack Chrome Extension' description="A chrome extension that return the weather status in real-time base on the given city's name. This is a 24h project for FullyHack" url='/' tools=''/>
-            <ProjectItem img={aichatbotImg} title = 'BotPress Chatbot' description="AI Chatbot that use a given dataset along with clever prompt engineering to assist customers for a quicker and better customer service." url='/' tools=''/>
-            <ProjectItem img={gradebookImg} title = 'Gradebook Database' description="Database system designed to assist professors and students on keeping track of their courses, grades, class's session, and standing status." url='https://github.com/johnnyblaze1999/Gradebook-database' tools=''/>
+            <ProjectItem img={placeholderImg} title = 'Personal Portfolio' description='My personal portfolio webpage showcasing my skills, experiences, and projects.' url='/' tools='ReactJS, Vite, TailwindCSS'/>
+            <ProjectItem img={placeholderImg} title = 'Unity Game' description='A fun desktop game where the player dodges incoming projectiles and survive under the time limit.' url='/' tools='Unity, C#'/>
+            <ProjectItem img={placeholderImg} title = 'Fullyhack Chrome Extension' description="A chrome extension that return the weather status in real-time base on the given city's name. This is a 24h project for FullyHack" url='https://github.com/Fullyhacks2024/Fullyhacks_Project' tools='HTML, CSS, Javascript'/>
+            <ProjectItem img={aichatbotImg} title = 'BotPress Chatbot' description="AI Chatbot that use a given dataset along with clever prompt engineering to assist customers for a quicker and better customer service." url='https://github.com/johnnyblaze1999/CapstonePrototype' tools='Javascript, BotPress, Dataset, AI'/>
+            <ProjectItem img={gradebookImg} title = 'Gradebook Database' description="Database system designed to assist professors and students on keeping track of their courses, grades, class's session, and standing status." url='https://github.com/johnnyblaze1999/Gradebook-database' tools='MySQL, OpenOffice, HTML, CSS'/>
             <ProjectItem img={placeholderImg} title = 'Hotel Webpage' description="A simple styled Hotel website to support guests on seeking information and hotel's schedule." url='/' tools=''/>
         </div>
     </div>
