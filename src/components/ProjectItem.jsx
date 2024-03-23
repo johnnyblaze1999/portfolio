85ac1038ca8b58481b3ce50793636d5098a5bf1c
Kevin Nguyen <johnnyblaze1999.sg@gmail.com>
2024-03-22 18:40:37 -0700

    Added react-awesome-reveal

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,35 +1,42 @@
 import React from 'react'
+import { Fade } from 'react-awesome-reveal';
 
 const ProjectItem = ({img, title, description, url, tools}) => {
   const toolList = tools.split(', ');
 
   return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-tr from-gray-400 to-emerald-400 hover:scale-110'>
-        <img src={img} alt='/' className='rounded-xl object-cover h-96 w-96 group-hover:opacity-10 '/>
-        <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-            <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
-                {title}
-            </h3>
-            <div className='flex justify-center space-x-2'>
-              {toolList.map((tool, index) => (
-                <button key={index} className='px-2 my-2 py-1 bg-rose-400 text-white text-sm rounded-full'>{tool}</button>
-              ))}
-            </div>
-            <a href={url} target='_blank' rel='noopener norefferer'>
-              <p className='text-center p-1 my-2 rounded-lg bg-white text-gray-700 font-bold cursor-pointer transition hover:scale-110 ease-in active:scale-90'>
-                View Project
-              </p>
-            </a>
-            <details className="text-white text-center font-semibold rounded-lg hover:cursor-pointer" close>
-              <summary className="text-sm select-none">
-                Details
-              </summary>
-              <div className="mt-1 text-sm">
-                {description}
+    <Fade direction='up' triggerOnce>
+      <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-tr from-gray-400 to-emerald-400 hover:scale-110 ease-in duration-150'>
+          <img src={img} alt='/' className='rounded-xl object-cover h-96 w-full group-hover:opacity-10 '/>
+          <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+              <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
+                  {title}
+              </h3>
+              <div className='flex justify-center space-x-2'>
+                {toolList.map((tool, index) => (
+                  <button key={index} className='px-2 my-2 py-1 bg-rose-400 text-white text-sm rounded-full'>{tool}</button>
+                ))}
               </div>
-            </details>
-        </div>
-    </div>
+              <a href={url} target='_blank' rel='noopener norefferer'>
+                <div className='flex justify-center'>
+                  <p className='relative overflow-hidden inline-block text-center p-1 px-4 my-2 rounded-lg bg-white text-gray-700 font-bold cursor-pointer transition ease-in hover:scale-110 active:scale-90'>
+                    <span className='relative z-10'>View Project</span>
+                    
+                  </p>
+                </div>
+              </a>
+
+              <details className="text-white text-center font-semibold rounded-lg hover:cursor-pointer" close>
+                <summary className="text-sm select-none">
+                  Details
+                </summary>
+                <div className="mt-1 text-sm">
+                  {description}
+                </div>
+              </details>
+          </div>
+      </div>
+    </Fade>
   )
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,8 @@ import ProjectItem from './ProjectItem'
 import placeholderImg from '../assets/placeholder.png'
 import gradebookImg from '../assets/gradebookProject.png'
 import aichatbotImg from '../assets/aichatbotProject.png'
+import portfolioImg from '../assets/portfolio.png'
+import { Fade } from 'react-awesome-reveal';
 
 /** import propertyImg from '../assets/property.jpg'
 import cryptoImg from '../assets/crypto.jpg'
@@ -12,17 +14,22 @@ import twitchImg from '../assets/twitch.jpg'**/
 const Projects = () => {
   return (
     <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
-        <h1 className='text-4xl font-bold text-center text-[#001b5e]'>
-            Projects
-        </h1>
-        <p className='text-center py-8'>
-            Here are some of the projects that I've done over the years. Feel free to check around more projects in my Github repo.
-        </p>
+        <Fade direction='down' triggerOnce>
+          <h1 className='text-4xl font-bold text-center text-[#001b5e]'>
+              Projects
+          </h1>
+        </Fade>
+        <Fade direction='up' triggerOnce>
+          <p className='text-center text-lg py-8'>
+            Here are some of the projects that I've done over the years, including what I have learned in class and my personal projects. Feel free to check around for more projects in my Github repo.
+          </p>
+        </Fade>
+        
         <div className='grid sm:grid-cols-2 gap-12'>
-            <ProjectItem img={placeholderImg} title = 'Personal Portfolio' description='My personal portfolio webpage showcasing my skills, experiences, and projects.' url='/' tools='ReactJS, Vite, TailwindCSS'/>
+            <ProjectItem img={portfolioImg} title = 'Personal Portfolio' description='My personal portfolio webpage showcasing my skills, experiences, and projects.' url='/' tools='ReactJS, Vite, TailwindCSS'/>
             <ProjectItem img={placeholderImg} title = 'Unity Game' description='A fun desktop game where the player dodges incoming projectiles and survive under the time limit.' url='/' tools='Unity, C#'/>
             <ProjectItem img={placeholderImg} title = 'Fullyhack Chrome Extension' description="A chrome extension that return the weather status in real-time base on the given city's name. This is a 24h project for FullyHack" url='https://github.com/Fullyhacks2024/Fullyhacks_Project' tools='HTML, CSS, Javascript'/>
-            <ProjectItem img={aichatbotImg} title = 'BotPress Chatbot' description="AI Chatbot that use a given dataset along with clever prompt engineering to assist customers for a quicker and better customer service." url='https://github.com/johnnyblaze1999/CapstonePrototype' tools='Javascript, BotPress, Dataset, AI'/>
+            <ProjectItem img={aichatbotImg} title = 'BotPress Chatbot' description="AI Chatbot that use a given dataset along with clever prompt engineering to assist customers for a quicker and better customer service." url='https://github.com/johnnyblaze1999/CapstonePrototype' tools='Javascript, BotPress, AI'/>
             <ProjectItem img={gradebookImg} title = 'Gradebook Database' description="Database system designed to assist professors and students on keeping track of their courses, grades, class's session, and standing status." url='https://github.com/johnnyblaze1999/Gradebook-database' tools='MySQL, OpenOffice, HTML, CSS, PHP'/>
             <ProjectItem img={placeholderImg} title = 'Hotel Webpage' description="A simple styled Hotel website to support guests on seeking information and hotel's schedule." url='/' tools=''/>
         </div>
