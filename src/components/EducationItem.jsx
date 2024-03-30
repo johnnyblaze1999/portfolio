f401821ffdfc053787114a402f42f79e4ab544f2
Kevin Nguyen <johnnyblaze1999.sg@gmail.com>
2024-03-29 23:03:23 -0700

    Minor code change

--- src/components/EducationItem.jsx
+++ src/components/EducationItem.jsx
@@ -6,7 +6,7 @@ const EducationItem = ({year, degree, school, duration, details}) => {
     <ol className='flex flex-col md:flex-row relative border-l border-stone-200'>
         <li className='mb-10 ml-4'>
             <div className='absolute w-3 h-3 bg-[#D4A09F] rounded-full mt-1.5 -left-1.5 border-white'/>
-            <Slide direction='right'>
+            <Slide direction='right' triggerOnce>
                 <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
                     <span className='inline-block px-2 py-1 font-semibold text-white bg-[#001b5e] rounded-md'>{year}</span>
                     <span className='text-lg font-semibold text-[#809D9D]'>{degree}</span>
